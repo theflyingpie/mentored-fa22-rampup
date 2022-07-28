4644c38f1e8b3214c2392a0533bb0b8fd42b4fa1
Claire Thibodeaux <clairethibodeaux@berkeley.edu>
2022-07-27 20:49:57 -0400

    Create outline for mentored rampup

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  alert("This is the correct way to do js ☺️ 😊🥰👉👈");
+}
+
+function submit() {
+    username = document.getElementById('username');
+    email = document.getElementById('email');
+    password = document.getElementById('password');
+    alert('Username:', username.value, 'Email:', email.value, 'Password:', password.value);
+    return false;
+}
