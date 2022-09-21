ab8fc62b176e780710a6f1c8bb0a3f4d92a559b9
Sophia Sunkin <sophiasunkin@berkeley.edu>
2022-09-20 22:05:09 -0700

    finished v1

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
-function myFunction() {
-  alert("This is the correct way to do js ☺️ 😊🥰👉👈");
+var currPiece;
+var otherPiece;
+
+var moves = 0;
+
+window.onload = function() {
+  let pieces = [];
+  for (let i = 0; i < 25; i ++) {
+    pieces.push(i);
+  }
+
+  //swapping pieces to randomize the order
+  for (let i = 0; i < 25; i ++) {
+    j = pieces[Math.floor(Math.random() * pieces.length)]
+    let tmp = pieces[i];
+    pieces[i] = pieces[j];
+    pieces[j] = tmp;
+  }
+
+  //adding all pieces to board
+  for (let i = 0; i < 25; i ++) {
+    let piece = document.createElement("img");
+    piece.setAttribute("draggable", "true");
+    piece.src = "./imageonline/" + pieces[i] + ".jpg";
+    piece.addEventListener("dragstart", dragStart);
+    piece.addEventListener("dragover", dragOver);
+    piece.addEventListener("dragenter", dragEnter);
+    piece.addEventListener("drop", drop);
+    piece.addEventListener("dragEnd", dragEnd);
+    document.getElementById("board").append(piece);
+  }
 }
 
-function submit() {
-    username = document.getElementById('username');
-    email = document.getElementById('email');
-    password = document.getElementById('password');
-    alert('Username:', username.value, 'Email:', email.value, 'Password:', password.value);
-    return false;
+function dragStart(e) {
+  currPiece = this;
+  console.log("drag start")
+  console.log(currPiece.src)
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  otherPiece = e.target;
+  console.log("dropped")
+  console.log(otherPiece)
+  let currImg = currPiece.src;
+  let otherImg = otherPiece.src;
+  currPiece.src = otherImg;
+  otherPiece.src = currImg;
+  moves += 1;
+  document.getElementById("moves").innerText = moves;
+}
+
+function dragEnd() {
+  
+  consolelog("bruh")
 }
